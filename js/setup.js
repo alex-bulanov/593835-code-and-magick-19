b261d224c6d3fd38af7dbf5718bb46c1ddf0d88b
AlexBulanov <alex-bulanov.mail@yandex.ru>
2020-01-25 10:39:41 +0300

    Создание рандомных персонажей

---
+++ js/setup.js
@@ -0,0 +1,42 @@
+'use strict';
+
+var names = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var surnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+var coatColors = ['rgb (101, 137, 164)', 'rgb (241, 43, 107)', 'rgb (146, 100, 161)', 'rgb (56, 159, 117)', 'rgb (215, 210, 55)', 'rgb (0, 0, 0)'];
+var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
+
+var getRandomNumber = function (arr) {
+  var max = arr.length - 1;
+  var min = 0;
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+var creatingWizard = function (namesArr, surnamesArr, coatColorsArr, eyesColorsArr) {
+  var object = {
+    name: namesArr[getRandomNumber(namesArr)],
+    surname: surnamesArr[getRandomNumber(surnamesArr)],
+    coatColor: coatColorsArr[getRandomNumber(coatColorsArr)],
+    eyesColor: eyesColorsArr[getRandomNumber(eyesColorsArr)]
+  };
+  return object;
+};
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var setupSimilar = document.querySelector('.setup-similar');
+setupSimilar.classList.remove('hidden');
+
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+for (var i = 0; i < 4; i++) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+  var currentWizard = creatingWizard(names, surnames, coatColors, eyesColors);
+  wizardElement.querySelector('.setup-similar-label').textContent = currentWizard.name + ' ' + currentWizard.surname;
+  wizardElement.querySelector('.wizard-coat').style.fill = currentWizard.coatColor.slice(0, 3) + currentWizard.coatColor.slice(4);
+  wizardElement.querySelector('.wizard-eyes').style.fill = currentWizard.eyesColor;
+  similarListElement.appendChild(wizardElement);
+
+}
+
